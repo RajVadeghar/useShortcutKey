a497cf91d1698cececb468a963d025b65380ad0b
RajVadeghar <instigator0002@gmail.com>
2022-05-18 13:29:20 +0530

    version 1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # use-shortcut-key
 
-> 
+>
 
 [![NPM](https://img.shields.io/npm/v/use-shortcut-key.svg)](https://www.npmjs.com/package/use-shortcut-key) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
@@ -13,17 +13,26 @@ npm install --save use-shortcut-key
 ## Usage
 
 ```jsx
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import { useMyHook } from 'use-shortcut-key'
+import { useShortcutKeys } from "use-shortcut-key";
 
 const Example = () => {
-  const example = useMyHook()
+  const handleShortCut = () => {
+    console.log("Shortcut keys are used");
+  };
+  useShortcutKeys(["Control", "k", "s"], handleShortCut);
   return (
-    <div>{example}</div>
-  )
-}
+    <div>
+      <button onClick={handleShortCut}>Handle Shortcut using button</button>
+    </div>
+  );
+};
 ```
+
+As you've seen in above example. You can use button onClick to run a function (or) you can use this hook to create shortcut for handling the function
+
+> **_NOTE:_** The shortcuts you use will over ride default behaviour of browser. If you use "Control", "p" inside of this hook it overrides default behaviour of web browser which is print
 
 ## License
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,106 @@
-import * as React from 'react'
+import { useEffect, useCallback, useRef } from "react";
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState({
-    counter: 0
-  })
-
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval)
+const overrideSystemHandling = (e) => {
+  if (e) {
+    if (e.preventDefault) e.preventDefault();
+    if (e.stopPropagation) {
+      e.stopPropagation();
+    } else if (window.event) {
+      window.event.cancelBubble = true;
     }
-  }, [])
+  }
+};
 
-  return counter
-}
+const BLACKLISTED_DOM_TARGETS = ["TEXTAREA", "INPUT"];
+
+const overrideSystem = true;
+
+export const useShortcutKeys = (keys, callback) => {
+  if (!Array.isArray(keys))
+    throw new Error(
+      "The first parameter to `useKey` must be an ordered array of `KeyboardEvent.key` strings."
+    );
+
+  if (!keys.length)
+    throw new Error(
+      "The first parameter to `useKey` must contain atleast one `KeyboardEvent.key` string."
+    );
+
+  if (!callback || typeof callback !== "function")
+    throw new Error(
+      "The second parameter to `useKey` must be a function that will be envoked when the keys are pressed."
+    );
+
+  const callbackRef = useRef(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  });
+
+  const heldKeys = useRef([]);
+
+  const flushHeldKeys = useCallback(() => {
+    heldKeys.current = [];
+  }, []);
+
+  const keyDownListener = useCallback(
+    (event) => {
+      const { key, target, repeat } = event;
+      if (repeat) return;
+      if (BLACKLISTED_DOM_TARGETS.includes(target.tagName)) return;
+      if (!keys.includes(key)) return;
+
+      if (overrideSystem) {
+        overrideSystemHandling(event);
+      }
+
+      const nextHeldKeys = [...new Set([...heldKeys.current, key])];
+
+      if (nextHeldKeys.join() === keys.join()) {
+        callback(keys);
+        flushHeldKeys();
+        return;
+      }
+
+      heldKeys.current = nextHeldKeys;
+      return;
+    },
+    [keys, callback, flushHeldKeys]
+  );
+
+  const keyupListener = useCallback(
+    (event) => {
+      const { key, target } = event;
+      if (BLACKLISTED_DOM_TARGETS.includes(target.tagName)) return;
+      if (!keys.includes(key)) return;
+
+      const raisedKeyHeldIndex = heldKeys.current.indexOf(key);
+      if (!(raisedKeyHeldIndex >= 0)) return;
+
+      let nextHeldKeys = [];
+      let loopIndex;
+      for (loopIndex = 0; loopIndex < heldKeys.current.length; ++loopIndex) {
+        if (loopIndex !== raisedKeyHeldIndex) {
+          nextHeldKeys.push(heldKeys.current[loopIndex]);
+        }
+      }
+      heldKeys.current = nextHeldKeys;
+
+      return false;
+    },
+    [keys]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyDownListener);
+    document.addEventListener("keyup", keyupListener);
+    return () => {
+      document.removeEventListener("keydown", keyDownListener);
+      document.removeEventListener("keyup", keyupListener);
+    };
+  }, [keyDownListener, keyupListener, keys]);
+
+  return {
+    flushHeldKeys,
+  };
+};
